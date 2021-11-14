c19dc4aca51c4632670cf3c52b4e6186af65fbc3
Tarun <tarunspartan1234@outlook.com>
2021-11-15 01:02:49 +0530

    updated head & favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Tarun M</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
