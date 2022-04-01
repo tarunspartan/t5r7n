1027e11d9f8ba9d4ac3338a8286be4fdc3427d0b
Tarun <tarunspartan1234@outlook.com>
2022-04-01 19:25:18 +0530

    added emoji for no reason

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center', fontSize:'30px' }}>
-            April Foools
+            April Foools 🤣
         </div>
     )
 }
