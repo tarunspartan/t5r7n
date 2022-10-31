faeb113a4f4ac90762486939ac924f504d768eec
Tarun <tarunspartan1234@outlook.com>
2022-11-01 02:10:33 +0530

    changed

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center', fontSize:'30px' }}>
-            April Foools 🤣
+        <div style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center', fontSize: '30px' }}>
+            Tarun M
+            <br />
+            Currently working as React dev
         </div>
     )
 }
